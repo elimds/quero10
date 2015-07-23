cdf9250062d636538c4cce06b9cf9316b5df98c7
Paulo Mesquita Jr <paulo4dm@hotmail.com>
2015-07-23 02:41:59 +0000

    alteração na forma de login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
       belongs_to :person
       has_secure_password
-      validates :email, uniqueness: true, :presence =>  true
+      validates :login, uniqueness: true, :presence =>  true
       validates :password, :presence => true,
            :confirmation => true,
            :length => {:within => 6..40},
